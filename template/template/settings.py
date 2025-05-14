98c5af22924738348eae0dbcba45e3ccdb230976
Pham Minh Long <phamminhlong200112@gmail.com>
2025-05-15 01:19:14 +0700

    feat: config db

--- template/template/settings.py
+++ template/template/settings.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'template.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'template_db',
+        'USER': 'long',
+        'PASSWORD': '123123',
     }
 }
 
